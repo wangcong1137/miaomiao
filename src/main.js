e291df14a1c76a6cb54aaeab45c65fa7afa80bdc
王聪 <wangcong403@163.com>
2020-06-01 00:34:03 +0800

    add setData

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routers'
 import store from './stores'
+//引入axios
+import axios from 'axios'
+Vue.prototype.axios=axios
+Vue.filter('setWH',(url,urg)=>{
+  return url.replace(/w\.h/,urg)
+})
 
 Vue.config.productionTip = false
 
