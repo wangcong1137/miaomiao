954305a5310ce0f2db36aa5c801993ad1c4ab038
王聪 <wangcong403@163.com>
2020-06-06 16:36:37 +0800

    detailPage

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,70 +3,34 @@
         <Header title="影片详情">
             <i class="iconfont icon-right" @touchstart='handleToBack'></i>
         </Header>
-        <div id="content" class="contentDetail">
+        <Loading v-if="this.isLoading" />
+        <div v-else id="content" class="contentDetail">
             <div class="detail_list">
                 <div class="detail_list_bg"></div>
                 <div class="detail_list_filter"></div>
                 <div class="detail_list_content">
                     <div class="detail_list_img">
-                        <img src="/images/movie_1.jpg" alt="">
+                        <img :src="detailMovie.img|setWH('148.208')" alt="">
                     </div>
                     <div class="detail_list_info">
-                        <h2>无名之辈</h2>
-                        <p>A Cool Fish</p>
-                        <p>9.2</p>
-                        <p>剧情,喜剧,犯罪</p>
-                        <p>中国大陆 / 108分钟</p>
-                        <p>2018-11-16大陆上映</p>
+                        <h2>{{detailMovie.nm}}</h2>
+                        <p>{{detailMovie.enm}}</p>
+                        <p>{{detailMovie.sc}}</p>
+                        <p>{{detailMovie.cat}}</p>
+                        <p>{{detailMovie.src}}/{{detailMovie.dur}}</p>
+                        <p>{{detailMovie.pubDesc}}</p>
                     </div>
                 </div>
             </div>
             <div class="detail_intro">
-                <p>在一座山间小城中，一对低配劫匪、一个落魄的泼皮保安、一个身体残疾却性格彪悍的残毒舌女以及一系列生活在社会不同轨迹上的小人物，在一个貌似平常的日子里，因为一把丢失的老枪和一桩当天发生在城中的乌龙劫案，从而被阴差阳错地拧到一起，发生的一幕幕令人啼笑皆非的荒诞喜剧。</p>
+                <p>{{detailMovie.dra}}</p>
             </div>
-            <div class="detail_player swiper-container">
+            <div class="detail_player swiper-container" ref="detail_player">
                 <ul class="swiper-wrapper">
-                    <li class="swiper-slide">
+                    <li class="swiper-slide" v-for="(v,i) in detailMovie.photos" :key="i" >
                         <div>
-                            <img src="/images/person_1.webp" alt="">
+                            <img :src="v|setWH('140.127')" alt="">
                         </div>
-                        <p>陈建斌</p>
-                        <p>马先勇</p>
-                    </li>
-                    <li class="swiper-slide">
-                        <div>
-                            <img src="/images/person_1.webp" alt="">
-                        </div>
-                        <p>陈建斌</p>
-                        <p>马先勇</p>
-                    </li>
-                    <li class="swiper-slide">
-                        <div>
-                            <img src="/images/person_1.webp" alt="">
-                        </div>
-                        <p>陈建斌</p>
-                        <p>马先勇</p>
-                    </li>
-                    <li class="swiper-slide">
-                        <div>
-                            <img src="/images/person_1.webp" alt="">
-                        </div>
-                        <p>陈建斌</p>
-                        <p>马先勇</p>
-                    </li>
-                    <li class="swiper-slide">
-                        <div>
-                            <img src="/images/person_1.webp" alt="">
-                        </div>
-                        <p>陈建斌</p>
-                        <p>马先勇</p>
-                    </li>
-                    <li class="swiper-slide">
-                        <div>
-                            <img src="/images/person_1.webp" alt="">
-                        </div>
-                        <p>陈建斌</p>
-                        <p>马先勇</p>
                     </li>
                 </ul>
             </div>
@@ -80,10 +44,33 @@ export default {
     components :{
         Header
     },
+    data(){
+        return {
+            detailMovie:{},
+            isLoading:true
+        }
+    },
+    props:["movieId"],
     methods:{
         handleToBack(){
             this.$router.back()
         }
+    },
+    mounted(){
+        this.axios.get("/api/detailmovie?movieId="+this.movieId).then((res)=>{
+            var msg = res.data.msg
+            if(msg === 'ok'){
+                this.detailMovie = res.data.data.detailMovie
+                    this.isLoading = false
+                this.$nextTick(()=>{
+                    new Swiper(this.$refs.detail_player,{
+                        slidesPerView:'auto',
+                        freeMode:true,
+                        freeModesticky:true
+                    })
+                })
+            }
+        })
     }
 }
 </script>
@@ -104,4 +91,9 @@ export default {
 .detail_list .detail_list_info{ margin-top: 20px;}
 .detail_list .detail_list_info h2{ font-size: 20px; color:white; font-weight: normal; line-height: 40px;}
 .detail_list .detail_list_info p{ color:white; line-height: 20px; font-size: 14px; color:#ccc;}
+#content .detail_intro{ padding: 10px;}
+#content .detail_player{ margin:20px;}
+.detail_player .swiper-slide{ width:70px; margin-right: 20px; text-align: center; font-size: 14px;}
+.detail_player .swiper-slide img{ width:100%; margin-bottom: 5px;}
+.detail_player .swiper-slide p:nth-of-type(2){ color:#999;}
 </style>
